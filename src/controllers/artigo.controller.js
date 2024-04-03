9b852a92a6286e3d5eba15546293d168ca53b31b
Zuk3s <felipemarcedo_@hotmail.com>
2024-04-03 14:44:26 -0300

    Refactor (artigos.controller) : Adiciona paginação à função getArtigos.
    A função getArtigos foi alterada para incluir paginação. Agora, a função aceita dois parâmetros opcionais, pagina e limite, que permitem que os usuários recuperem uma lista de artigos em páginas específicas.

--- src/controllers/artigo.controller.js
+++ src/controllers/artigo.controller.js
@@ -1,53 +1,73 @@
-import { Artigo } from '../models/Artigo.js';
+import { Artigo } from "../models/Artigo.js";
 
+export const getArtigos = async (req, res) => {
+  try {
+    let { pagina = 1, limite = 3 } = req.query;
 
-export const getArtigos = async () => {
-    const artigos = await Artigo.findAll();
-    return artigos;
-}
+    pagina = parseInt(pagina);
+    limite = parseInt(limite);
+
+    const artigos = await Artigo.find()
+      .populate("subconteudo")
+      .skip((pagina - 1) * limite)
+      .limit(limite);
+
+    if (!artigos || artigos.length == 0) {
+      return res.status(404).json({ message: "Nenhum artigo encontrado" });
+    }
+
+    res.status(200).json(artigos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const createArtigo = async (req, res) => {
-    try {
-        const artigo = req.body;
-        if (artigo.titulo === '' || artigo.conteudo === '') 
-            return res.status(400).json({ error: 'Título e conteúdo são obrigatórios' });
-        const novoArtigo = await Artigo.create(artigo);
-        await novoArtigo.save();
-        res.status(201).json(novoArtigo);
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao criar artigo' });
-    }
-}
+  try {
+    const artigo = req.body;
+    if (artigo.titulo === "" || artigo.conteudo === "")
+      return res
+        .status(400)
+        .json({ error: "Título e conteúdo são obrigatórios" });
+    const novoArtigo = await Artigo.create(artigo);
+    await novoArtigo.save();
+    res.status(201).json(novoArtigo);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar artigo" });
+  }
+};
 
 export const updateArtigo = async (req, res) => {
-    try {
-        const artigoId = req.params.id;
-        const novoArtigo = req.body;
+  try {
+    const artigoId = req.params.id;
+    const novoArtigo = req.body;
 
-        const artigo = await Artigo.findByIdAndUpdate(artigoId, novoArtigo, { new: true });
-        if (!artigo) {
-            return res.status(404).json({ error: 'Artigo não encontrado' });
-        }
-
-        await artigo.update(novoArtigo);
-        await artigo.save();
-
-        res.status(201).json(artigo);
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao atualizar artigo' });
+    const artigo = await Artigo.findByIdAndUpdate(artigoId, novoArtigo, {
+      new: true,
+    });
+    if (!artigo) {
+      return res.status(404).json({ error: "Artigo não encontrado" });
     }
-}
+
+    await artigo.update(novoArtigo);
+    await artigo.save();
+
+    res.status(201).json(artigo);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao atualizar artigo" });
+  }
+};
 
 export const deleteArtigo = async (req, res) => {
-    try {
-        const artigoId = req.params.id;
+  try {
+    const artigoId = req.params.id;
 
-        const artigo = await Artigo.findByIdAndDelete(artigoId);
-        if (!artigo) {
-            return res.status(404).json({ error: 'Artigo não encontrado' });
-        }
-        res.status(200).json({ message: 'Artigo deletado com sucesso' });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao deletar artigo' });
+    const artigo = await Artigo.findByIdAndDelete(artigoId);
+    if (!artigo) {
+      return res.status(404).json({ error: "Artigo não encontrado" });
     }
-}
+    res.status(200).json({ message: "Artigo deletado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao deletar artigo" });
+  }
+};
