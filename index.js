dd2ca0532895cf2e5688b83c7c87a347928bf6f1
Zuk3s <felipemarcedo_@hotmail.com>
2024-04-03 14:50:12 -0300

    Refactor (index) : Alteração nas rotas get e post.
    A rota raiz foi alterada para retornar os dados dos Artigos em vez de "Hello World!". A rota /artigos foi adicionada para criar novos artigos.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import express from 'express';
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+import {
+  getArtigos,
+  createArtigo,
+} from "./src/controllers/artigo.controller.js";
 
 const app = express();
 app.use(express.json());
@@ -10,20 +12,20 @@ dotenv.configDotenv();
 
 //ROUTES
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", getArtigos);
 
-app.post('/artigos') //completar daqui
+app.post("/artigos", createArtigo); //completar daqui
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log('Connected to MongoDB in port');
-  app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB in port");
+    app.listen(port, () => {
+      console.log(`Listening on http://localhost:${port}`);
+    });
   })
-}).catch((error) => {
-  console.log('MongoDB connection error:', error);
-});
-
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
