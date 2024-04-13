b6437e337aa4e430c55cf1b4ed2ea9fd496c21a5
Zuk3s <felipemarcedo_@hotmail.com>
2024-04-13 02:30:18 -0300

    :recycle: :sparkles: feat(index): importação de novas bobliotecas, funcionalidade de armazenamento de imagem, adicionada as rotas com e sem foto.

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
-import {
-  getArtigos,
-  createArtigo,
-} from "./src/controllers/artigo.controller.js";
+import multer from "multer";
+import path from "path";
+import { register } from "./src/controllers/usuario.controller.js";
+import { createPost } from "./src/controllers/artigo.controller.js";
+import { verificarToken } from "./src/middleware/verificarToken.js";
+import artigosRota from "./src/routes/artigos.routes.js";
+import usuariosRota from "./src/routes/usuarios.router.js";
 
+// CONFIGURAÇÕES
 const app = express();
 app.use(express.json());
 dotenv.configDotenv();
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-//ROUTES
+// ARMAZEM DE FOTOS
+const armazem = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ armazem });
 
-app.get("/", getArtigos);
+//ROTAS COM FOTO
+app.post("/usuario/registrar", upload.single("foto"), register);
+app.post("/posts", verificarToken, upload.single("foto"), createPost);
 
-app.post("/artigos", createArtigo); //completar daqui
+//ROTAS
+app.use("/artigo", artigosRota);
+app.use("/usuario", usuariosRota);
 
+// SERVER
 const port = process.env.PORT || 3000;
 
 mongoose
