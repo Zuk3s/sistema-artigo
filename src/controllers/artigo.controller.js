02abaaa5bf3b8cf77a65cc166197a670df13c0de
Zuk3s <felipemarcedo_@hotmail.com>
2024-04-16 00:28:44 -0300

    :pencil: style(atigo.controller): Adição de ";" no final da função getArtgo.

--- src/controllers/artigo.controller.js
+++ src/controllers/artigo.controller.js
@@ -6,12 +6,12 @@ export const getArtigo = async (req, res) => {
     if (!artigo) {
       return res.status(404).json({ message: "Artigo não encontrado" });
     }
-    
+
     res.status(200).json(artigo);
   } catch (error) {
-    res.status(500).json({ message: "Artigo não encontrado"})
+    res.status(500).json({ message: "Artigo não encontrado" });
   }
-}
+};
 
 export const getArtigos = async (req, res) => {
   try {
@@ -66,7 +66,6 @@ export const updateArtigo = async (req, res) => {
       return res.status(404).json({ error: "Artigo não encontrado" });
     }
 
-    await artigo.update(novoArtigo);
     await artigo.save();
 
     res.status(201).json(artigo);
