961ab4de374993b51cbef89b32ab21dce70653ce
Zuk3s <felipemarcedo_@hotmail.com>
2024-04-16 00:26:53 -0300

    :white_check_mark: feat(usuarios.router): Rotas para acesso ao getUsuarios e PutUsuario

--- src/routes/usuarios.router.js
+++ src/routes/usuarios.router.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { login } from "../controllers/usuario.controller.js";
+import {
+  login,
+  getUsuarios,
+  putUsuario,
+} from "../controllers/usuario.controller.js";
+import { verificaAdmin } from "../middleware/admin.js";
 
 const rota = express.Router();
 
+// GET /usuario
+rota.get("/", verificaAdmin, getUsuarios);
+
+// POST /usuario/login
 rota.post("/login", login);
+
+// PUT /usuario/:id
+rota.put("/:id", verificaAdmin, putUsuario);
 
 export default router;
